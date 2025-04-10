a998a6c26c6c2491deccd21fe5a7751bcc745339
Devocolypse <72180176+Devocolypse@users.noreply.github.com>
2025-04-10 09:23:29 -0700

    feat: Add math operations and determiner

---
+++ script.js
@@ -0,0 +1,35 @@
+// Calculator display holders
+let firstNum = '';
+let secondNum = '';
+let operator = '';
+
+// Basic math operations
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+// Determine correct operation to call
+function operate(firstNum, secondNum, operator) {
+  switch (operator) {
+    case '+':
+      return add(firstNum, secondNum);
+    case '-':
+      return subtract(firstNum, secondNum);
+    case '*':
+      return multiply(firstNum, secondNum);
+    case '/':
+      return divide(firstNum, secondNum);
+  }
+}
